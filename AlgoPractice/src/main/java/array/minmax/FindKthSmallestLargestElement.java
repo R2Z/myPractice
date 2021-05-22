382267021bfe13344e2717c1009be82342b5bf3e
rpande46 <hi2rajnish@gmail.com>
2021-05-22 10:43:26 +0530

    sorting quick and merge

--- AlgoPractice/src/main/java/array/minmax/FindKthSmallestLargestElement.java
+++ AlgoPractice/src/main/java/array/minmax/FindKthSmallestLargestElement.java
@@ -31,15 +31,12 @@ public class FindKthSmallestLargestElement {
     }
 
     private static int findKmin(int[] arr, int k) {
-        Queue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if(o1 > o2)
-                    return -11;
-                if(o1 < o2)
-                    return 1;
-                else return 0;
-            }
+        Queue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> {
+            if(o1 > o2)
+                return -11;
+            if(o1 < o2)
+                return 1;
+            else return 0;
         });
         for (int val : arr) {
             maxHeap.add(val);
